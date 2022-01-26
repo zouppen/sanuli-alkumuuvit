388a4ed595c7e7faa7a0a394a83391f243fe4b5d
Joel Lehtonen <joel.lehtonen@iki.fi>
2022-01-26 16:46:16 +0200

    Filter interesting words out of the dictionary. Don't yet crunch

--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@ import GHC.IO (evaluate)
 import GHC.Exts (sortWith)
 import System.Environment (getArgs)
 import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 import Wordlist
 
 main = do
@@ -19,19 +20,28 @@ main = do
 
   let sanuliWords = filter (liftM2 (&&) isSanuliWord (hasLength wordLen)) words
       freqList = toFreqList $ frequency $ concat sanuliWords
-      ourLetters = map fst $ take (wordsToFind*wordLen) freqList
+      (keepThese, dropThese) = splitAt (wordsToFind*wordLen) freqList
+      ourLetters = S.fromList $ map fst keepThese
+      sanuliWordMap = toWordMap sanuliWords
+      ourWordMap = M.filterWithKey (\k _ -> filterPopular wordLen ourLetters k) sanuliWordMap
 
   putStrLn $
     "Keeping only Sanuli words... " ++ show (length sanuliWords) ++
     " words left"
-  putStr $
-    "Calculating frequency map of " ++ show wordLen ++
-    "-length words...\n" ++ freqShow freqList
-  putStrLn $ "Our letters of interest: " ++ ourLetters
+  putStrLn $ "Calculating frequency map of " ++ show wordLen ++ "-length words..."
+  putStr $ freqShow keepThese
+  putStrLn "    -- demotion zone --"
+  putStr $ freqShow dropThese
+  putStrLn $ "Distinct sequences... " ++ show (M.size sanuliWordMap)
+  putStrLn $ "Relevant sequences... " ++ show (M.size ourWordMap)
 
 freqShow :: [(Char, Int)] -> String
 freqShow list = unlines $ map line list
   where line (char, count) = "    " ++ [char] ++ ": " ++ show count
+
+filterPopular :: Int -> S.Set Char -> String -> Bool
+filterPopular wordLen ourLetters distinct =
+  length distinct == wordLen && all (`S.member` ourLetters) distinct
 
 {-
 -- |Just report what we're doing.
