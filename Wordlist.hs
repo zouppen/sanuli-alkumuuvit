cd63cf3ee2f246a228daa54ba153ac2031283656
Joel Lehtonen <joel.lehtonen@iki.fi>
2022-01-27 21:55:56 +0200

    Improve types

--- Wordlist.hs
+++ Wordlist.hs
@@ -7,10 +7,10 @@ import Data.List (sort, group)
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 
-type DistinctLetters = String
-type Anagrams = S.Set String
-type FreqMap = M.Map Char Int
-type WordMap = M.Map DistinctLetters Anagrams
+type Letters  = String                 -- ^Set of letters the word consists
+type Anagrams = S.Set String           -- ^Set of words which are anagrams
+type FreqMap  = M.Map Char Int         -- ^Character frequency map
+type WordMap  = M.Map Letters Anagrams -- ^Map of all words (grouped by anagrams)
 
 -- |Load Kotus words from their XML file as a list of strings.
 loadKotusWords :: String -> IO [String]
@@ -23,8 +23,8 @@ toWordMap :: Foldable t => t String -> WordMap
 toWordMap words = project toLetterSet words
 
 -- |Convert word to sorted distinct letter sequence
-toLetterSet :: String -> DistinctLetters
-toLetterSet = map head . group . sort
+toLetterSet :: String -> Letters
+toLetterSet = S.toList . S.fromList
 
 -- |Has specific length
 hasLength :: Int -> String -> Bool
