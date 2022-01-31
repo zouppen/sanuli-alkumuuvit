bcf2ab4ed327c16b8547084b5a02458678cb5a9f
Joel Lehtonen <joel.lehtonen@iki.fi>
2022-02-01 01:10:40 +0200

    Get rid of qualified A, make it readable

--- src/SanuliExtractor.hs
+++ src/SanuliExtractor.hs
@@ -2,35 +2,35 @@
 module SanuliExtractor where
 
 import Control.Applicative
-import qualified Data.Attoparsec.ByteString as A
+import Data.Attoparsec.ByteString (Parser, Result, anyWord8, word8, many1, sepBy1, parseWith, maybeResult)
 import qualified Data.ByteString as B
 import qualified Data.Set as S
 import qualified Data.Text as T
 import Data.Function (on)
-import Data.List
-import System.IO
+import Data.List (maximumBy)
+import System.IO (withFile, IOMode(ReadMode))
 import Utf8Parser
 
 -- |Skips characters until parser succeeds
-skipGarbage :: A.Parser a -> A.Parser a
-skipGarbage p = p <|> (A.anyWord8 >> skipGarbage p)
+skipGarbage :: Parser a -> Parser a
+skipGarbage p = p <|> (anyWord8 >> skipGarbage p)
 
 -- |Go through the file and find all possible Sanuli lists
-bestSanuliList :: A.Parser [T.Text]
+bestSanuliList :: Parser [T.Text]
 bestSanuliList = best <$> many (skipGarbage sanuliList)
   where best = maximumBy (compare `on` length)
 
 -- |List of Sanuli words
-sanuliList :: A.Parser [T.Text]
-sanuliList = sanuliString `A.sepBy1` A.word8 0x0A
+sanuliList :: Parser [T.Text]
+sanuliList = sanuliString `sepBy1` word8 0x0A
 
 -- |Parses non-empty Sanuli string
-sanuliString :: A.Parser T.Text
-sanuliString = A.many1 sanuliChar >>= validate lenCheck >>= pure . T.pack
+sanuliString :: Parser T.Text
+sanuliString = many1 sanuliChar >>= validate lenCheck >>= pure . T.toLower . T.pack
   where lenCheck x = length x == 5 || length x == 6
 
 -- |Takes only Sanuli letter
-sanuliChar :: A.Parser Char
+sanuliChar :: Parser Char
 sanuliChar = utf8char >>= validate isSanuliLetter 
   where isSanuliLetter = flip S.member $ S.fromList $ ['A'..'Z'] ++ "ÅÄÖ"
 
@@ -41,6 +41,13 @@ validate f a = if f a
                then pure a
                else empty
 
+-- |Parse Sanuli file and try to dig the words from there
+parseSanuliWasm :: FilePath -> IO [T.Text]
+parseSanuliWasm file = do
+  r <- parseFile bestSanuliList file
+  maybe err pure $ maybeResult r
+    where err = fail "Unable to parse Sanuli data"
+
 -- |Apply parser to file contents.
-parseFile :: A.Parser a -> FilePath -> IO (A.Result a)
-parseFile p f = withFile f ReadMode $ \h -> A.parseWith (B.hGetSome h 1024) p B.empty
+parseFile :: Parser a -> FilePath -> IO (Result a)
+parseFile p f = withFile f ReadMode $ \h -> parseWith (B.hGetSome h 1024) p B.empty
